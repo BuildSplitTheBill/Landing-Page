a02dfb8cecd584ce84338b6ef9122a241b2a664c
Zooheck <davidscottflack@gmail.com>
2018-12-12 15:12:28 -0600

    (STRETCH) finish about carousel functionality

--- Assets/about-page.js
+++ Assets/about-page.js
@@ -7,6 +7,9 @@ class Carousel {
        this.currentIndex = 0;
        this.currentImage = this.images[this.currentIndex];
        this.currentImage.style.display = 'block';
+       //NEW STUFF
+       this.bios = document.querySelectorAll('.bio-item');
+       this.currentBio = this.bios[this.currentIndex];
        this.leftButton.addEventListener('click', () => this.previousImage());
        this.rightButton.addEventListener('click', () => this.nextImage());
     }
@@ -20,7 +23,7 @@ class Carousel {
         if (this.currentIndex > 4) {
             this.currentIndex = 0;
         }
-        this.images[this.currentIndex].style.display = 'block';
+        this.changeBio();
      }
      nextImage() {
         const images = document.querySelectorAll('.carousel img');
@@ -32,61 +35,14 @@ class Carousel {
         if (this.currentIndex > 4) {
             this.currentIndex = 0;
         }
+        this.changeBio();
+     }
+     changeBio() {
         this.images[this.currentIndex].style.display = 'block';
+        this.bios.forEach(bio => bio.classList.remove('bio-selected'));
+        this.currentBio = this.bios[this.currentIndex]
+        this.currentBio.classList.add('bio-selected');
      }
 }
 
 let carousel = document.querySelectorAll('.carousel').forEach(carousel => new Carousel(carousel));
-
-class TabLink {
-    constructor(element) {
-      // Assign this.element to the passed in DOM element
-  
-      this.element = element;
-      // Get the custom data attribute on the Link
-      
-      //this.data = this.element.dataset.tab; // this is just a number
-      this.data = this.element.dataset.tab;
-      // Using the custom data attribute get the associated Item element
-      this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
-      
-      // Using the Item element, create a new instance of the TabItem class
-      this.tabItem = new TabItem(this.itemElement);
-      
-      // Add a click event listener on this instance, calling the select method on click
-      this.element.addEventListener('click', () => this.select());
-    };
-  
-    select() {
-      // Get all of the elements with the tabs-link class
-      const links = document.querySelectorAll('.tabs-link');
-      // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-      Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
-  
-      // Add a class named "tabs-link-selected" to this link
-      this.element.classList.add('tabs-link-selected');
-      
-      // Call the select method on the item associated with this link
-      this.tabItem.select();
-    }
-  }
-  
-  class TabItem {
-    constructor(element) {
-      // Assign this.element to the passed in element
-      this.element = element;
-    }
-  
-    select() {
-      // Select all ".tabs-item" elements from the DOM
-      const items = document.querySelectorAll(".tabs-item");
-  
-      // Remove the class "tabs-item-selected" from each element
-      //items.classList.remove("tabs-item-selected");
-      items.forEach(item => item.classList.remove("tabs-item-selected"));
-      // Add a class named "tabs-item-selected" to this element
-      this.element.classList.add("tabs-item-selected");
-    }
-  }
-
-  let links = document.querySelectorAll('.bio-item').forEach(tab => new TabLink(tab));
